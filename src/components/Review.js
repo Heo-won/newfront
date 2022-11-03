53575d508a7fcc379f483f5a9c34adab94e85a41
1 <106522988+Heo-won@users.noreply.github.com>
2022-11-04 04:25:46 +0900

    리뷰나만의음료추가

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../style/review.css";
 
 export default function Review(props) {
-  const { rating, content } = props;
+  const { rating, content, userEmail, del, _id, product, productId } = props;
+  const [update, setUpdate] = useState(false);
+
+  const navigate = useNavigate();
+
+  const deleteReivew = async () => {
+    const resDel = await fetch("http://localhost:5000/api/product/delete", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ _id, product }),
+    });
+    if (resDel.status === 200) {
+      alert(await resDel.json());
+      window.location.replace(`/api/compare/product/${productId}`);
+    } else {
+      alert("통신 이상");
+    }
+  };
+
   return (
     <div className="review-rating">
-      <span>{content} </span>
+      <span>{userEmail}</span>
+      <span>{content} &nbsp;</span>
       <span>
         <i
           className={
             rating >= 1
               ? "fa fa-star"
               : rating >= 0.5
-                ? "fa fa-star-half-o"
-                : "fa fa-star-o"
+              ? "fa fa-star-half-o"
+              : "fa fa-star-o"
           }
         ></i>
       </span>
@@ -23,8 +43,8 @@ export default function Review(props) {
             rating >= 2
               ? "fa fa-star"
               : rating >= 1.5
-                ? "fa fa-star-half-o"
-                : "fa fa-star-o"
+              ? "fa fa-star-half-o"
+              : "fa fa-star-o"
           }
         ></i>
       </span>
@@ -34,8 +54,8 @@ export default function Review(props) {
             rating >= 3
               ? "fa fa-star"
               : rating >= 2.5
-                ? "fa fa-star-half-o"
-                : "fa fa-star-o"
+              ? "fa fa-star-half-o"
+              : "fa fa-star-o"
           }
         ></i>
       </span>
@@ -45,8 +65,8 @@ export default function Review(props) {
             rating >= 4
               ? "fa fa-star"
               : rating >= 3.5
-                ? "fa fa-star-half-o"
-                : "fa fa-star-o"
+              ? "fa fa-star-half-o"
+              : "fa fa-star-o"
           }
         ></i>
       </span>
@@ -56,11 +76,16 @@ export default function Review(props) {
             rating >= 5
               ? "fa fa-star"
               : rating >= 4.5
-                ? "fa fa-star-half-o"
-                : "fa fa-star-o"
+              ? "fa fa-star-half-o"
+              : "fa fa-star-o"
           }
         ></i>
       </span>
+      {del && (
+        <span className="delete" onClick={() => deleteReivew()}>
+          삭제
+        </span>
+      )}
     </div>
   );
 }
